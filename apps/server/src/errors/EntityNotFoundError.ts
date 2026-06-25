57f32ebb744265013c4fe231c2d69abcbf91a30e
Sarrnot <sarrnot@sarrnot.com>
2026-06-25 16:49:32 +0200

    refactor(server): accept drizzle Table in EntityNotFoundError

--- apps/server/src/errors/EntityNotFoundError.ts
+++ apps/server/src/errors/EntityNotFoundError.ts
@@ -1,3 +1,4 @@
+import { getTableName, Table } from "drizzle-orm";
 import { DomainError } from "./DomainError.js";
 
 /** An entity addressed by id does not exist. */
@@ -5,7 +6,8 @@ export class EntityNotFoundError extends DomainError {
     readonly entity: string;
     readonly id: string;
 
-    constructor(entity: string, id: string) {
+    constructor(table: Table, id: string) {
+        const entity = getTableName(table);
         super(`${entity} ${id} not found`);
         this.entity = entity;
         this.id = id;
